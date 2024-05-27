3e9a339f3c99310a22a886c9be233cf77aeef2ec
Wilu187 <wojciech.wilowski@gmail.com>
2024-05-27 18:53:53 +0200

    dodana funkcja vote do obrazkow jako jeden wynik a nie dwa osobne

--- src/components/Mem.js
+++ src/components/Mem.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Mem = ({ title, img }) => {
+  const [votes, setVotes] = useState(0);
+
+  const handleVote = (value) => {
+    setVotes((prevVotes) => prevVotes + value);
+  };
+
   return (
     <div className="mem">
       <h2>{title}</h2>
       <img src={img} alt={title} />
+      <div>
+        <button onClick={() => handleVote(1)}>+</button>
+        <span>{votes}</span>
+        <button onClick={() => handleVote(-1)}>-</button>
+      </div>
     </div>
   );
 };
